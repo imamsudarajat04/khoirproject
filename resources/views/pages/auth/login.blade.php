798ece741543aa794d69ee7e7b468ea87e8331fb
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-08 17:34:38 +0700

    Vendor

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -41,7 +41,7 @@
 
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <p class="text-center small">Enter your email & password to login</p>
                   </div>
 
                   <form action="{{ route('login.store') }}" method="POST" class="row g-3 needs-validation" novalidate>
