a3eeaa0b417b84caf90a6dafde90e780ac35127a
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-14 19:22:10 +0700

    Update Model GlobalSetting

--- app/GlobalSetting.php
+++ app/GlobalSetting.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class GlobalSetting extends Model
 {
     protected $table = 'global_settings';
-    protected $fillable = ['navbar_title', 'font_size_title', 'font_size_subtitle', 'page_banner', 'page_banner_about', 'page_banner_testimonials'];
+    
+    protected $fillable = [
+        'navbar_title', 
+        'font_size_title', 
+        'font_size_subtitle', 
+        'page_banner', 
+        'page_banner_about', 
+        'page_banner_testimonials',
+        'subtitle_services',
+        'subtitle_team',
+        'subtitle_faq',
+        'subtitle_contact',
+    ];
 }
