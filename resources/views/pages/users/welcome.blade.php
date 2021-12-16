a4be80de238c43f809a8d75d5e7421d136af7bc5
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-17 01:33:13 +0700

    fix landingpage

--- resources/views/pages/users/welcome.blade.php
+++ resources/views/pages/users/welcome.blade.php
@@ -57,7 +57,7 @@
   
           <div class="row">
             <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative" data-aos="fade-right">
-              <a href="https://www.youtube.com/watch?v=J3sxLuDKjiY" class="glightbox play-btn mb-4"></a>
+              <a href="{{ $abouts->link_youtube }}" class="glightbox play-btn mb-4"></a>
             </div>
   
             <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
