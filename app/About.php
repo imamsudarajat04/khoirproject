a4d0b1fc84d8f9e38d1a798056eeaca5d1f46451
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-17 01:15:34 +0700

    Model About us

--- app/About.php
+++ app/About.php
@@ -13,9 +13,12 @@ class About extends Model
         'subtitle',
         'title_point_one',
         'subtitle_point_one',
+        'icon_point_one',
         'title_point_two',
         'subtitle_point_two',
+        'icon_point_two',   
         'title_point_three',
-        'subtitle_point_three'
+        'subtitle_point_three',
+        'icon_point_three',
     ];
 }
