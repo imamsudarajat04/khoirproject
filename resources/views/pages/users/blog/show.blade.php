82b00ffe11f6bf67bfc01718034547b6b66ef12c
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-28 00:33:41 +0700

    Search di Halaman Blog

--- resources/views/pages/users/blog/show.blade.php
+++ resources/views/pages/users/blog/show.blade.php
@@ -120,9 +120,9 @@
 
                 <h3 class="sidebar-title">Search</h3>
                 <div class="sidebar-item search-form">
-                    <form action="">
-                    <input type="text">
-                    <button type="submit"><i class="bi bi-search"></i></button>
+                    <form action="{{ route('blog.search') }}" method="GET">
+                        <input type="text" name="search">
+                        <button type="submit"><i class="bi bi-search"></i></button>
                     </form>
                 </div><!-- End sidebar search formn-->
 
