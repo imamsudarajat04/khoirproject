4e119b645e3207639f6f516b654390920a14ef5e
imamsudarajat04 <imamsudrajat707@gmail.com>
2021-12-28 01:33:02 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use UniSharp\LaravelFilemanager\Lfm;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::resource('/', 'BerandaController');
 
 Route::get('/login', 'LoginController@index')->name('login');
